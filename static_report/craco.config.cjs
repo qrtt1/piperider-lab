4f057cfd89a55c4ebd54680aa48142c8ef210fe0
Ching Yi, Chan <qrtt1@infuseai.io>
2023-08-18 21:38:11 +0800

    is this working?

--- static_report/craco.config.cjs
+++ static_report/craco.config.cjs
@@ -1,34 +1,52 @@
-const CracoEsbuildPlugin = (async () => await import('craco-esbuild'))();
+const CracoEsbuildPlugin = (async () => await import("craco-esbuild"))();
+const webpack = require('webpack');
 const event = process.env.npm_lifecycle_event;
 
-if (event === 'serve:single' || event === 'serve:comparison') {
-  const { readFileSync, existsSync } = require('fs');
-  const { resolve } = require('path');
-  const FILENAME_SINGLE = 'run.json';
-  const FILENAME_COMPARISON = 'comparison_data.json';
+if (event === "serve:single" || event === "serve:comparison") {
+  const { readFileSync, existsSync } = require("fs");
+  const { resolve } = require("path");
+  const FILENAME_SINGLE = "run.json";
+  const FILENAME_COMPARISON = "comparison_data.json";
 
-  const report_type = event.split(':')[1];
+  const report_type = event.split(":")[1];
   process.env.REACT_APP_SINGLE_REPORT_DATA_JSON = {};
   process.env.REACT_APP_COMPARISON_REPORT_DATA_JSON = {};
 
   // Load report data from current directory
-  if (report_type === 'single') {
+  if (report_type === "single") {
     const singleReportPath = resolve(FILENAME_SINGLE);
     if (existsSync(singleReportPath)) {
       process.env.REACT_APP_SINGLE_REPORT_DATA_JSON = JSON.stringify(
-        JSON.parse(Buffer.from(readFileSync(singleReportPath, 'utf8'))),
+        JSON.parse(Buffer.from(readFileSync(singleReportPath, "utf8")))
       );
     }
-  } else if (report_type === 'comparison') {
+  } else if (report_type === "comparison") {
     const comparisonReportPath = resolve(FILENAME_COMPARISON);
     if (existsSync(comparisonReportPath)) {
       process.env.REACT_APP_COMPARISON_REPORT_DATA_JSON = JSON.stringify(
-        JSON.parse(Buffer.from(readFileSync(comparisonReportPath, 'utf8'))),
+        JSON.parse(Buffer.from(readFileSync(comparisonReportPath, "utf8")))
       );
     }
   }
 }
 
 module.exports = {
-  plugins: [{ plugin: CracoEsbuildPlugin }],
+  webpack: {
+    configure: {
+      optimization: {
+        runtimeChunk: false,
+        splitChunks: {
+          chunks(chunk) {
+            return false;
+          }
+        }
+      },
+      plugins: [
+        new webpack.optimize.LimitChunkCountPlugin({
+          maxChunks: 1,
+        })
+      ]
+    }
+  },
+  plugins: [{ plugin: CracoEsbuildPlugin }]
 };
